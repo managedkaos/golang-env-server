1bf79d2611ac8217ec5975e7fe80e7c663953e01
Michael <managedkaos@users.noreply.github.com>
2020-12-10 13:34:25 -0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM golang:1.13-alpine AS builder
+RUN apk --update add ca-certificates git make g++
+#ENV GO111MODULE=on
+WORKDIR /app
+COPY . .
+ARG COMMIT_HASH
+ENV COMMIT_HASH=${COMMIT_HASH}
+ARG BUILD_DATE
+ENV BUILD_DATE=${BUILD_DATE}
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
+    go build \
+    -o app
+FROM golang:1.13-alpine
+WORKDIR /app
+COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
+COPY --from=builder /app/app .
+EXPOSE 3000
+CMD [ "./app" ]
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"strings"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<title>Environment Variables</title>"))
+	w.Write([]byte("<h1>Environment Variables</h1>"))
+	w.Write([]byte("<table>"))
+	w.Write([]byte("<tr><th style='text-align:left'>KEY</th><th style='text-align:left'>VALUE</th></tr>"))
+	for _, element := range os.Environ() {
+		variable := strings.Split(element, "=")
+		w.Write([]byte("<tr><td style='text-align:left'>" + variable[0] + "</td><td style='text-align:left'>" + variable[1] + "</td></tr>"))
+	}
+	w.Write([]byte("</table>"))
+
+}
+
+func main() {
+	port := "3000"
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
